b8ed6b7c6fb43fcffdd88dcb8c41f7605994773f
Pavel <pavelvf@inbox.ru>
2016-11-24 08:29:27 +0300

    #8 Отлажена тестовая конфигурация обработки заявки на ЛИ, полученной из МИС

--- ariadna-war/src/test/java/mis/integration/ariadna/war/ReportIntegrationContextTest.java
+++ ariadna-war/src/test/java/mis/integration/ariadna/war/ReportIntegrationContextTest.java
@@ -1,5 +1,7 @@
 package mis.integration.ariadna.war;
 
+import mis.lis.prescription.PrescriptionDTO;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +13,12 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import java.io.File;
+import java.io.StringWriter;
+import java.util.Calendar;
 
 /**
  * Тестируем настройку схемы обработки файла с протоколом ЛИ
@@ -24,10 +31,32 @@ public class ReportIntegrationContextTest extends AbstractAriadnaTest {
   @Qualifier("lisReportFileInputChannel")
   private DirectChannel lisReportFileInputChannel;
 
+  @Autowired
+  @Qualifier("misPrescriptionStringChannel")
+  private DirectChannel misPrescriptionStringChannel;
+
+  private Marshaller marshaller;
+
+  @Before
+  public void initMarshaller() throws JAXBException {
+    JAXBContext jc = JAXBContext.newInstance(PrescriptionDTO.class);
+    marshaller = jc.createMarshaller();
+  }
+
   @Test
   public void testLisReportFileTransformationAlgo() {
     File file = getResourceFile("reports/15350001.xml");
     Message<File> message = MessageBuilder.withPayload(file).build();
     lisReportFileInputChannel.send(message);
+  }
+
+  @Test
+  public void testMisPrescriptionTransformationAlgo() throws JAXBException {
+    PrescriptionDTO dto = new PrescriptionDTO();
+    dto.setDate(Calendar.getInstance());
+    StringWriter writer = new StringWriter();
+    marshaller.marshal(dto, writer);
+    Message<String> message = MessageBuilder.withPayload(writer.toString()).build();
+    misPrescriptionStringChannel.send(message);
   }
 }
--- ariadna/src/main/java/mis/integration/ariadna/Transformer.java
+++ ariadna/src/main/java/mis/integration/ariadna/Transformer.java
@@ -2,9 +2,11 @@ package mis.integration.ariadna;
 
 import mis.integration.ariadna.data.Observation;
 import mis.integration.ariadna.data.ReportGroup;
+import mis.lis.prescription.PrescriptionDTO;
 import mis.lis.report.Report;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 /**
@@ -19,6 +21,12 @@ public class Transformer {
       result.add(new Report());
       result.add(new Report());
     }
+    return result;
+  }
+
+  public Observation transformToObservation(PrescriptionDTO prescription) {
+    final Observation result = new Observation();
+    result.setId(Calendar.getInstance().getTimeInMillis());
     return result;
   }
 }
