f1971942a76f1f3db2558acbde93fd5aca6ebee5
Feller Pavel <SBT-Feller-PV@mail.ca.sbrf.ru>
2016-11-25 16:24:23 +0300

    #9 Реализован Md5Transformer. Получение md5-digest для объекта типа String

---
+++ ariadna-war/src/main/java/mis/integration/ariadna/Md5Transformer.java
@@ -0,0 +1,19 @@
+package mis.integration.ariadna;
+
+import org.springframework.messaging.Message;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Получение md5-digest для строки
+ */
+public class Md5Transformer {
+  public String transform(Message<String> message) throws NoSuchAlgorithmException {
+    MessageDigest digest = MessageDigest.getInstance("MD5");
+    digest.update(message.getPayload().getBytes());
+    final String md5 = new BigInteger(1, digest.digest()).toString();
+    return "MD5 (" + message.getHeaders().get("output-file-name") +".XML) = " + md5;
+  }
+}
--- ariadna-war/src/main/java/mis/integration/ariadna/RequestFileNameGenerator.java
+++ ariadna-war/src/main/java/mis/integration/ariadna/RequestFileNameGenerator.java
@@ -3,8 +3,6 @@ package mis.integration.ariadna;
 import org.springframework.integration.file.FileNameGenerator;
 import org.springframework.messaging.Message;
 
-import java.util.Calendar;
-
 /**
  * Алгоритм генерации имени файла
  */
@@ -17,17 +15,7 @@ public class RequestFileNameGenerator implements FileNameGenerator {
 
   @Override
   public String generateFileName(Message<?> message) {
-    final Calendar date = Calendar.getInstance();
-    int year = date.get(Calendar.YEAR);                //порядковый номер года
-    year -= (year > 2000) ? 2000 : 0;
-    final int days = date.get(Calendar.DAY_OF_YEAR);   //порядковый номер дня в году
-    final int number = getMessageNumber(message);      //порядковый номер сообщения в течение дня (в 36-чной системе)
-    final String name = String.format("%02d%03d%03d", year, days, number);
-
+    final String name = (String) message.getHeaders().get("output-file-name");
     return name + "." + extension;
-  }
-
-  private int getMessageNumber(Message<?> message) {
-    return 0;
   }
 }
