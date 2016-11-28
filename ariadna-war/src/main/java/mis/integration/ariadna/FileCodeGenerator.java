94ac2bbdad6b43386d0b2fe15100fe8089c23a16
Pavel <pavelvf@inbox.ru>
2016-11-28 20:53:01 +0300

    #14 Генерация имени файла порядковый номер в 36-ричном формате

--- ariadna-war/src/main/java/mis/integration/ariadna/FileCodeGenerator.java
+++ ariadna-war/src/main/java/mis/integration/ariadna/FileCodeGenerator.java
@@ -14,13 +14,24 @@ public class FileCodeGenerator {
   /** Время последнего запроса */
   private int lastAccessDay = 0;
 
+  /**
+   * Расчет кода сообщения (идентификатора файла)
+   */
   public String computeValue(Message<?> message) {
     final Calendar date = Calendar.getInstance();
     int year = date.get(Calendar.YEAR);                //порядковый номер года
     year -= (year > 2000) ? 2000 : 0;
     final int days = date.get(Calendar.DAY_OF_YEAR);   //порядковый номер дня в году
-    final int number = getMessageNumber(days);         //порядковый номер сообщения в течение дня (в 36-чной системе)
-    return String.format("%02d%03d%03d", year, days, number);
+    final String number = getFormattedNumber(getMessageNumber(days));  //порядковый номер сообщения в течение дня (в 36-чной системе)
+    return String.format("%02d%03d", year, days) + number;
+  }
+
+  /** Перевод порядкового номера в 36-ричную систему с лидирующими нулями */
+  private static String getFormattedNumber(int num) {
+    String result = Integer.toString(num, 36);
+    while (result.length() <= 3)
+      result = "0" + result;
+    return result;
   }
 
   private int getMessageNumber(int accessDay) {
