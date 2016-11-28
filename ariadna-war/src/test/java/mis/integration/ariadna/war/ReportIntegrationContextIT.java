dc1dcc8b61fd03284fbedde059df55b0a4950c8d
Pavel <pavelvf@inbox.ru>
2016-11-29 00:04:39 +0300

    Удалил лишнее из теста, переименовал в интеграционный тест

--- ariadna-war/src/test/java/mis/integration/ariadna/war/ReportIntegrationContextIT.java
+++ ariadna-war/src/test/java/mis/integration/ariadna/war/ReportIntegrationContextIT.java
@@ -1,9 +1,5 @@
 package mis.integration.ariadna.war;
 
-import mis.lis.prescription.LabPrescriptionDTO;
-import mis.lis.prescription.PatientDTO;
-import mis.lis.prescription.PrescriptionDTO;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,12 +11,8 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
 import java.io.File;
-import java.io.StringWriter;
-import java.util.Calendar;
 
 /**
  * Тестируем настройку схемы обработки файла с протоколом ЛИ
@@ -28,7 +20,7 @@ import java.util.Calendar;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ActiveProfiles("dev")
 @ContextConfiguration("classpath:report-integration.xml")
-public class ReportIntegrationContextTest extends AbstractAriadnaTest {
+public class ReportIntegrationContextIT extends AbstractAriadnaTest {
   @Autowired
   @Qualifier("lisReportFileInputChannel")
   private DirectChannel lisReportFileInputChannel;
@@ -36,14 +28,6 @@ public class ReportIntegrationContextTest extends AbstractAriadnaTest {
   @Autowired
   @Qualifier("prescriptionFileInputChannel")
   private DirectChannel prescriptionFileInputChannel;
-
-  private Marshaller marshaller;
-
-  @Before
-  public void initMarshaller() throws JAXBException {
-    JAXBContext jc = JAXBContext.newInstance(PrescriptionDTO.class);
-    marshaller = jc.createMarshaller();
-  }
 
   @Test
   public void testLisReportFileTransformationAlgo() {
