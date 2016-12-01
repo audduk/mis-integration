9d9437fb0458b2b02934fd64b7d986b137b22194
Pavel <pavelvf@inbox.ru>
2016-12-02 00:04:27 +0300

    #17 Реализовать трансформер в zip-архив. Изменение реализации ZipTransformer под требования протокола МИС.  Обратить внимание, что можно укладывать только два файла - xml и pdf, не более.

--- ariadna-war/src/main/java/mis/integration/ariadna/file/ZipTransformer.java
+++ ariadna-war/src/main/java/mis/integration/ariadna/file/ZipTransformer.java
@@ -16,7 +16,6 @@
 
 package mis.integration.ariadna.file;
 
-import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.integration.file.FileHeaders;
@@ -28,18 +27,16 @@ import org.springframework.integration.zip.transformer.ZipResultType;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHandlingException;
 import org.springframework.util.Assert;
-import org.springframework.util.FileCopyUtils;
-import org.springframework.util.StringUtils;
 import org.zeroturnaround.zip.ByteSource;
-import org.zeroturnaround.zip.FileSource;
 import org.zeroturnaround.zip.ZipEntrySource;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.zip.Deflater;
 
 /**
+ * Special version for MIS protocol
+ *
  * {@link Transformer} implementation that applies a Zip transformation to the
  * message payload. Keep in mind that Zip entry timestamps are recorded only to
  * two 2 second precision:
@@ -61,7 +58,9 @@ public class ZipTransformer extends AbstractZipTransformer {
 
   private volatile int compressionLevel = Deflater.DEFAULT_COMPRESSION;
 
-  private volatile boolean useFileAttributes = true;
+  public ZipTransformer() {
+    zipResultType = ZipResultType.BYTE_ARRAY;
+  }
 
   /**
    * Sets the compression level. Default is {@link Deflater#DEFAULT_COMPRESSION}.
@@ -74,20 +73,10 @@ public class ZipTransformer extends AbstractZipTransformer {
   }
 
   /**
-   * Specifies whether the name of the file shall be used for the
-   * zip entry.
-   *
-   * @param useFileAttributes Defaults to true if not set explicitly
-   */
-  public void setUseFileAttributes(boolean useFileAttributes) {
-    this.useFileAttributes = useFileAttributes;
-  }
-
-  /**
    * The payload may encompass the following types:
    * <p>
    * <ul>
-   * <li>{@link File}
+   * <li>{@link File} - unsupported!
    * ...<li>{@link String}
    * ...<li>byte[]
    * ...<li>{@link Iterable}
@@ -105,20 +94,7 @@ public class ZipTransformer extends AbstractZipTransformer {
       final Object zippedData;
       final String baseFileName = this.fileNameGenerator.generateFileName(message);
 
-      final String zipEntryName;
-      final String zipFileName;
-
-      if (message.getHeaders().containsKey(ZipHeaders.ZIP_ENTRY_FILE_NAME)) {
-        zipEntryName = (String) message.getHeaders().get(ZipHeaders.ZIP_ENTRY_FILE_NAME);
-      } else {
-        zipEntryName = baseFileName;
-      }
-
-      if (message.getHeaders().containsKey(FileHeaders.FILENAME)) {
-        zipFileName = baseFileName;
-      } else {
-        zipFileName = baseFileName + ZIP_EXTENSION;
-      }
+      final String zipFileName = baseFileName + ZIP_EXTENSION;
 
       final Date lastModifiedDate;
 
@@ -131,37 +107,24 @@ public class ZipTransformer extends AbstractZipTransformer {
       java.util.List<ZipEntrySource> entries = new ArrayList<ZipEntrySource>();
 
       if (payload instanceof Iterable<?>) {
-        int counter = 1;
-
-        String baseName = FilenameUtils.getBaseName(zipEntryName);
-        String fileExtension = FilenameUtils.getExtension(zipEntryName);
-
-        if (StringUtils.hasText(fileExtension)) {
-          fileExtension = FilenameUtils.EXTENSION_SEPARATOR_STR + fileExtension;
-        }
-
         for (Object item : (Iterable<?>) payload) {
 
-          final ZipEntrySource zipEntrySource = createZipEntrySource(item, lastModifiedDate, baseName + "_"
-              + counter + fileExtension, this.useFileAttributes);
+          final ZipEntrySource zipEntrySource = createZipEntrySource(item, lastModifiedDate);
           if (logger.isDebugEnabled()) {
             logger.debug("ZipEntrySource path: '" + zipEntrySource.getPath() + "'");
           }
           entries.add(zipEntrySource);
-          counter++;
         }
       } else {
         final ZipEntrySource zipEntrySource =
-            createZipEntrySource(payload, lastModifiedDate, zipEntryName, this.useFileAttributes);
+            createZipEntrySource(payload, lastModifiedDate);
         entries.add(zipEntrySource);
       }
 
       final byte[] zippedBytes = SpringZipUtils.pack(entries, this.compressionLevel);
 
       if (ZipResultType.FILE.equals(this.zipResultType)) {
-        final File zippedFile = new File(this.workDirectory, zipFileName);
-        FileCopyUtils.copy(zippedBytes, zippedFile);
-        zippedData = zippedFile;
+        throw new RuntimeException("Unsupported payload type. Use standard zip-transformer");
       } else if (ZipResultType.BYTE_ARRAY.equals(this.zipResultType)) {
         zippedData = zippedBytes;
       } else {
@@ -178,31 +141,14 @@ public class ZipTransformer extends AbstractZipTransformer {
     }
   }
 
-  private ZipEntrySource createZipEntrySource(Object item,
-                                              Date lastModifiedDate, String zipEntryName, boolean useFileAttributes) {
+  private ZipEntrySource createZipEntrySource(Object item, Date lastModifiedDate) {
 
-    if (item instanceof File) {
-      final File filePayload = (File) item;
+    //Требование протокола ЛИС
+    final String zipEntryName = (item instanceof String) ? "manifest.xml" : "manifest.pdf";
 
-      final String fileName = useFileAttributes ? filePayload.getName() : zipEntryName;
+    if (item instanceof byte[] || item instanceof String) {
 
-      if (((File) item).isDirectory()) {
-        throw new UnsupportedOperationException("Zipping of directories is not supported.");
-      }
-
-      final FileSource fileSource = new FileSource(fileName, filePayload);
-
-      if (this.deleteFiles) {
-        if (!filePayload.delete() && logger.isWarnEnabled()) {
-          logger.warn("failed to delete File '" + filePayload + "'");
-        }
-      }
-
-      return fileSource;
-
-    } else if (item instanceof byte[] || item instanceof String) {
-
-      byte[] bytesToCompress = null;
+      final byte[] bytesToCompress;
 
       if (item instanceof String) {
         bytesToCompress = ((String) item).getBytes(this.charset);
@@ -213,7 +159,7 @@ public class ZipTransformer extends AbstractZipTransformer {
       return new ByteSource(zipEntryName, bytesToCompress, lastModifiedDate.getTime());
     } else {
       throw new IllegalArgumentException("Unsupported payload type. The only supported payloads are " +
-          "java.io.File, java.lang.String, and byte[]");
+          "java.lang.String, and byte[]");
     }
   }
 
