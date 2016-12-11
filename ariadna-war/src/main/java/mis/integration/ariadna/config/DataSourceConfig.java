cd6a44e0f603e5bec917dfec9c4a2a2dd9df14d6
Pavel <pavelvf@inbox.ru>
2016-12-11 15:42:50 +0300

    #19 Словари. Реализована конфигурация доступа к базе данных МИС.

---
+++ ariadna-war/src/main/java/mis/integration/ariadna/config/DataSourceConfig.java
@@ -0,0 +1,32 @@
+package mis.integration.ariadna.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class DataSourceConfig {
+  @Value("${jdbc.driver}")
+  private String driverClassName;
+  @Value("${jdbc.url}")
+  private String url;
+  @Value("${jdbc.username}")
+  private String username;
+  @Value("${jdbc.password}")
+  private String password;
+
+  @Bean
+  public DataSource dataSource() {
+    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    dataSource.setDriverClassName(driverClassName);
+    dataSource.setUrl(url);
+    dataSource.setUsername(username);
+    dataSource.setPassword(password);
+    return dataSource;
+  }
+}
